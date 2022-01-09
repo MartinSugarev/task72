aca6d04ae7f9d77508aeb267686b81d7df790d54
Martin <mdsugarev@gmail.com>
2022-01-09 10:29:37 +0200

    t

--- src/Tag.js
+++ src/Tag.js
@@ -8,9 +8,6 @@ return(
     <task className="tag"></task>
     <tags className="tag"></tags>
     <react className="tag"></react>
-  {props.tags.map((t) => {
-      return <t className="tag"></t>
-  })}
   </div>
 )
 
