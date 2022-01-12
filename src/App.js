ed94e5fa94684046a540ef1f883d053573f94cc5
Martin <mdsugarev@gmail.com>
2022-01-12 11:26:41 +0200

    h

--- src/App.js
+++ src/App.js
@@ -3,23 +3,7 @@ import Tags from './Tags'
 
 function App() {
   return (
-    <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-          <div className="tags">
-              <Tags tags={['martn', 'bozhidar']} />
-          </div>
-        </div>
-      </div>
-    </div>
-    
+      <Tags tags={["boomdotdev", "task", "tags", "react"]} />
   );
 }
 
--- src/Tags.js
+++ src/Tags.js
@@ -1,19 +1,14 @@
-import React,{ useRef } from 'react'
+import React  from 'react'
 
 export default function Tag({tags}){
 
-const tag = useRef()
-const tagEle = tag.current
-
-
-
-ReactDOM.render(<boomdotdev className="tag"></boomdotdev>, tagEle)
-ReactDOM.render(<task className="tag"></task>, tagEle)
-ReactDOM.render(<tags className="tag"></tags>, tagEle)
-ReactDOM.render(<react className="tag"></react>, tagEle)
 
 return(
-  <div ref={tag} className="tags"></div>
+  <div className="tags">
+    {tags.map((t) => {
+      return <div className="tag">{'#' + t}</div>
+    })}
+  </div>
 )
 
 }
